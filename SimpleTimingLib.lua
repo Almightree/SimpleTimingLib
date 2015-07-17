6ffd6a2d84abd6e60af10861b1a5a8000394d8bf
Almightree <almightree@gmail.com>
2015-07-17 14:55:53 -0500

    First Attempts

---
+++ SimpleTimingLib.lua
@@ -0,0 +1,36 @@
+local tasks = {}
+
+function SimpleTimingLib_Schedule(time, func, ...)
+	local t = {...}
+	t.func = func
+	t.time = GetTime() + time
+	table.insert(tasks, t)
+end
+
+local function onUpdate()
+	for i = #tasks, 1, -1 do
+		local val = tasks[i]
+		if val and val.time <= GetTime() then
+			table.remove(tasks, i)
+			val.func(unpack(val))
+		end
+	end
+end
+
+function SimpleTimingLib_Unschedule(func, ...)
+	for i = #tasks, 1, -1 do
+		local val = tasks[i]
+		if val.func == true
+			local matches = true
+			for i = 1, select("#", ...) do
+				if select(i, ...) ~= val[i] then
+				matches = false
+				break
+				end
+			end
+			if matches then
+				table.remove(tasks, i)
+			end
+		end
+	end
+end
